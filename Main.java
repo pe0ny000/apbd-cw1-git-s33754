2bf91bd67d6b71b5f4561d617dc555b8c8331cc8
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-03-12 20:27:11 +0100

    Print dodawania

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        
+
+        System.out.println(dodawanie(6,7));
 
     }
     public static int dodawanie(int a, int b){
